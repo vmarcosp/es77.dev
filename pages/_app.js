0f2969c245a2c2cb476829137827e72cb4d5b054
Gabriel Rubens <grsabreu@gmail.com>
2021-11-26 17:27:18 -0300

    fix: move fb pixel down below

--- pages/_app.js
+++ pages/_app.js
@@ -46,19 +46,6 @@ export default function App(props) {
           name="twitter:image"
           content="https://es77.dev/assets/cover.png"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-    !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');fbq('init', '446955093453188'); fbq('track', 'PageView');`,
-          }}
-        />
       </Head>
       <CourseJsonLd
         courseName="EcmaScript 2077"
@@ -176,6 +163,20 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
       />
 
       <ReScriptApp {...props} />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');fbq('init', '446955093453188'); fbq('track', 'PageView');`,
+          }}
+        />
     </>
   );
 }
